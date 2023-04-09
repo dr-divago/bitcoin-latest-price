419b6b0d91d451bbebe2788c5908d58ff6885966
dr-divago <matteo.pelliccia@gmail.com>
2023-04-09 19:15:36 +0200

    Fix configuration

--- web-service/src/main/java/PublicApiMain.java
+++ web-service/src/main/java/PublicApiMain.java
@@ -1,6 +1,8 @@
 
 import com.example.ConfigBuilder;
+import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,8 +17,9 @@ public class PublicApiMain {
       ConfigBuilder configBuilder = new ConfigBuilder(vertx);
         configBuilder.build().onSuccess(config -> {
             logger.info("Config file correctly loaded");
-            config.getServiceConfig().getWebServiceConfig().toJsonObject();
-            vertx.deployVerticle(new PublicApiVerticle())
+            JsonObject publicVerticleConfig = config.getServiceConfig().getWebServiceConfig().toJsonObject();
+
+            vertx.deployVerticle(new PublicApiVerticle(), new DeploymentOptions().setConfig(publicVerticleConfig))
                 .onSuccess(ok -> logger.info("Public Api Service running"))
                 .onFailure(error -> logger.error("Error starting PublicApi {}", error));
         }).onFailure(err -> logger.error("Error reading configuration!", err));
